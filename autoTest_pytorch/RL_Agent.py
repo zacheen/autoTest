19d3ae4dff8c24a0cb82ced98ea6166853710ed3
zacheen <zacheenoao@gmail.com>
2026-03-31 03:58:53 -0700

    add input residual + epsilon-greedy to transformer agent + clip

--- autoTest_pytorch/RL_Agent.py
+++ autoTest_pytorch/RL_Agent.py
@@ -1752,6 +1752,7 @@ class TransformerDiscreteAgent:
 
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.critic.parameters(), max_norm=1.0)
         self.critic_optimizer.step()
 
         # --- Actor update ---
